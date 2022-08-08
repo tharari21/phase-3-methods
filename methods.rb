656008f76fc114f817233fb8fb934732ac696248
Tomer Harari <tharari21@gmail.com>
2022-08-08 10:21:48 -0400

    commit tree c2b8fbd76abf3367f036e87f3a270e6e208d15a4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+# Your code here
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  num1 + num2;
+end
+
+
+def halve(number)
+    puts number.class
+    return nil if number.class != Integer
+
+    number / 2
+end
+
